92b9873dca5b68875ec987d49c71021a228d5595
Mohamed Lemine tedjih <59097385+Med-lemin@users.noreply.github.com>
2025-05-30 22:54:33 +0000

    new changes

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,15 +1,54 @@
-import React from "react";
+import React, { useState } from "react"; 
+import useGlobalReducer from "../hooks/useGlobalReducer";
+import { Link, useNavigate } from "react-router-dom";
+
 
 export const Input = () => {
+
+
+
+    const {store, dispatch} = useGlobalReducer();
+    const navigate = useNavigate();
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [address, setAddress] = useState("");
+    async function addNewContact() {
+        if (name === "" || phone === "" || email === "" || address === "") throw new Error (
+            "missing a field"
+        );
+        const requestBody = {
+            name: name, 
+            email: email, 
+            phone: phone,  
+            address: address, 
+        };
+        const response = await fetch(`${store.BASE_URL}/${store.SLUG}/contacts`, {
+            method: "POST", 
+            body: JSON.stringify(requestBody), 
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+        const body = await response.json();
+        if (!response.ok) throw new Error(`status:${response.status}, message: ${body}`);
+        setName("");
+        setPhone("");
+        setEmail(""); 
+        setAddress("");
+        navigate("/");
+    }
     return (
         <form className="contact-input-form">
             <div>
-               
                 <input
                     type="text"
                     name="fullName"
-                    placeholder=" Full name"
+                    placeholder=" full name"
                     required
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                    
                 />
             </div>
             <div>
@@ -19,6 +58,8 @@ export const Input = () => {
                     name="email"
                     placeholder=" Email"
                     required
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
                 />
             </div>
             <div>
@@ -28,6 +69,8 @@ export const Input = () => {
                     name="phone"
                     placeholder=" Phone number"
                     required
+                    value={phone}
+                    onChange={e => setPhone(e.target.value)}
                 />
             </div>
             <div>
@@ -37,6 +80,8 @@ export const Input = () => {
                     name="address"
                     placeholder="Address"
                     required
+                    value={address}
+                    onChange ={e => setAddress(e.target.value)}
                 />
             </div>
             <button type="submit" className="btn btn-primary">Save Contact</button>
